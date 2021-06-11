cfa99f1ee6404c6a27a6a4de0daf511403d7969c
Max Ooi Wei Xiang <wxo_rocket0123@hotmail.com>
2021-06-12 03:19:26 +0800

    rev 1

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 function compute() {
     //Get the values and calculate 
     var principal = parseFloat(document.getElementById("principal").value);
+	var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } else {
     var rate = parseFloat(document.getElementById("rate").value);
-    var years = parseInt(document.getElementById("years").value);
-    var interest = principal * years * rate / 100;
-    var yearInTheFuture = new Date().getFullYear() + years;
-    //Create the Interest text
-    document.getElementById("result").innerHTML = 
-		"If you deposit <b>"+principal+"</b>,<br\>at an interest rate of <b>"+rate+"</b>%,<br\>You will receive an amount of <b>"+interest+"</b><br\>in the year <b>"+yearInTheFuture+"</b>.<br\>";
+		var years = parseInt(document.getElementById("years").value);
+		var interest = principal * years * rate / 100;
+		var yearInTheFuture = new Date().getFullYear() + years;
+		//Create the Interest text
+		document.getElementById("result").innerHTML = 
+			"If you deposit <mark>"+principal+"</mark>,<br\>at an interest rate of <mark>"+rate+"</mark>%.<br\>You will receive an amount of <mark>"+interest+"</mark><br\>in the year <mark>"+yearInTheFuture+"</mark>.<br\>";
+	}
 }
 
 //update ther ate value
 function updateRate() {
-    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value+"%";
 }
 
 //Check for positive values
-function validateAmount() {
+/* function validateAmount() {
     var principal = document.getElementById("principal").value;
     var biggerThanZero = parseInt(principal) > 0;
     if (!biggerThanZero) {
@@ -24,4 +30,4 @@ function validateAmount() {
         document.getElementById("principal").focus();
     }
 
-}
+} */
